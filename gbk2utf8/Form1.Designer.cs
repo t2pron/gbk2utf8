c0c7993a751f559974d9806636587dc148a94128
mytuya <mytuya@qq.com>
2017-01-18 17:48:33 +0800

    提交gbk转utf8工具的代码。

---
+++ gbk2utf8/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace gbk2utf8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSourceFolderPath = new System.Windows.Forms.TextBox();
+            this.btnSelectSourceFolder = new System.Windows.Forms.Button();
+            this.btnSelectTargetFolder = new System.Windows.Forms.Button();
+            this.tbTargetFolderPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // tbSourceFolderPath
+            // 
+            resources.ApplyResources(this.tbSourceFolderPath, "tbSourceFolderPath");
+            this.tbSourceFolderPath.Name = "tbSourceFolderPath";
+            // 
+            // btnSelectSourceFolder
+            // 
+            resources.ApplyResources(this.btnSelectSourceFolder, "btnSelectSourceFolder");
+            this.btnSelectSourceFolder.Name = "btnSelectSourceFolder";
+            this.btnSelectSourceFolder.UseVisualStyleBackColor = true;
+            this.btnSelectSourceFolder.Click += new System.EventHandler(this.btnSelectSourceFolder_Click_1);
+            // 
+            // btnSelectTargetFolder
+            // 
+            resources.ApplyResources(this.btnSelectTargetFolder, "btnSelectTargetFolder");
+            this.btnSelectTargetFolder.Name = "btnSelectTargetFolder";
+            this.btnSelectTargetFolder.UseVisualStyleBackColor = true;
+            this.btnSelectTargetFolder.Click += new System.EventHandler(this.btnSelectTargetFolder_Click_1);
+            // 
+            // tbTargetFolderPath
+            // 
+            resources.ApplyResources(this.tbTargetFolderPath, "tbTargetFolderPath");
+            this.tbTargetFolderPath.Name = "tbTargetFolderPath";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // btnStart
+            // 
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSelectTargetFolder);
+            this.Controls.Add(this.tbTargetFolderPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelectSourceFolder);
+            this.Controls.Add(this.tbSourceFolderPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSourceFolderPath;
+        private System.Windows.Forms.Button btnSelectSourceFolder;
+        private System.Windows.Forms.Button btnSelectTargetFolder;
+        private System.Windows.Forms.TextBox tbTargetFolderPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ gbk2utf8/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gbk2utf8
+{
+    public partial class Form1 : Form
+    {
+        string mSrcPath;
+        string mDesPath;
+        public Form1()
+        {
+            InitializeComponent();
+            mSrcPath = "";
+            mDesPath = "";
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            mSrcPath = tbSourceFolderPath.Text;
+            mDesPath = tbTargetFolderPath.Text;
+            if (mSrcPath == "" || mDesPath == "")
+            {
+                MessageBox.Show("请选择文件夹！");
+                return;
+            }
+            if (!mSrcPath.EndsWith("\\"))
+            {
+                mSrcPath += "\\";
+            }
+            if (!mDesPath.EndsWith("\\"))
+            {
+                mDesPath += "\\";
+            }
+
+            //执行转换代码
+            Thread thread = new Thread(converTo);
+            thread.Start();
+        }
+
+        private void converTo()
+        { 
+            //获取路径中的文件、文件夹进行转换
+            if (!Directory.Exists(mSrcPath))
+            {
+                this.Invoke(new Action(() =>
+                {
+                    invokeMethod("原文件夹不存在！");
+                }));
+
+                return;
+            }
+            
+            gbk2utfFolder(mSrcPath, mDesPath);
+            this.Invoke(new Action(() =>
+            {
+                invokeMethod("转换完成");
+            }));
+                 
+        }
+
+        private void gbk2utfFolder(string src, string des) 
+        {
+            //获取文件夹中所有的文件 
+            DirectoryInfo srcFolder = new DirectoryInfo(src);
+            FileInfo[] files = srcFolder.GetFiles();   
+
+            //转换
+            foreach (FileInfo item in files)
+            {
+                string fileDes = des + item.Name;
+                gbk2utf8file(item.FullName, fileDes);
+            }
+
+            DirectoryInfo[] dirs = srcFolder.GetDirectories();
+            foreach (DirectoryInfo item in dirs)
+            {
+                string folderdes = des + item.Name + "//";
+                if (!Directory.Exists(folderdes))
+                {
+                    Directory.CreateDirectory(folderdes);
+                }
+                gbk2utfFolder(item.FullName, folderdes);
+            }
+        }
+
+        private void gbk2utf8file(string src, string des)
+        {
+            //var buffer = File.ReadAllBytes(src);
+            string strGbk = File.ReadAllText(src, Encoding.Default);
+            byte[] buffer = Encoding.GetEncoding("GBK").GetBytes(strGbk);
+            byte[] buffer2 = Encoding.Convert(Encoding.Default, Encoding.UTF8, buffer);
+            //buffer = Encoding.Convert(Encoding.GetEncoding("GBK"), Encoding.UTF8, buffer);
+            //if (!File.Exists(des))
+            //{
+            //    File.Create(des, );
+            //}
+            //File.WriteAllText(des, utf);
+            File.WriteAllBytes(des, buffer2);
+        }
+
+        private void btnSelectSourceFolder_Click_1(object sender, EventArgs e)
+        {
+             if (DialogResult.OK == folderBrowserDialog.ShowDialog())
+            {
+                tbSourceFolderPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void btnSelectTargetFolder_Click_1(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == folderBrowserDialog.ShowDialog())
+            {
+                tbTargetFolderPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void invokeMethod(string message)
+        {
+            MessageBox.Show(message);
+        } 
+    }
+}
